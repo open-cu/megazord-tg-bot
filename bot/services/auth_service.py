78201c3cee0578ee711e7e30555b11e05d372e9e
Pozitiv4500 <123630113+Pozitiv4500@users.noreply.github.com>
2024-08-21 17:46:42 +0300

    Add: fix errors (#3)

--- bot/services/auth_service.py
+++ bot/services/auth_service.py
@@ -7,7 +7,7 @@ from config import EMAIL, PASSWORD, BASE_URL
 async def get_token():
     async with httpx.AsyncClient() as client:
         try:
-            response = await client.post(BASE_URL+"signin", json={"email": EMAIL, "password": PASSWORD})
+            response = await client.post(BASE_URL+"auth/signin", json={"email": EMAIL, "password": PASSWORD})
             response.raise_for_status()
             data = response.json()
             return data.get("token")
