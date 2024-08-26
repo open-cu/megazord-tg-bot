ce89bd5d4b228e42b8674b71d5ec0ec67c94d82f
lysmux <kllraz@yandex.ru>
2024-08-26 11:40:29 +0300

    docker & ci

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
+[![codebeat badge](https://codebeat.co/badges/8c4d77ac-465a-42c3-a5fe-05a5224c0dac)](https://codebeat.co/projects/github-com-open-cu-megazord-tg-bot-main)
+[![codecov](https://codecov.io/gh/open-cu/megazord-tg-bot/graph/badge.svg?token=G0X8OIJB5L)](https://codecov.io/gh/open-cu/megazord-tg-bot)
+
+
 # megazord-tg-bot
 Платформа, которая позволяет администрировать командные соревнование в части создания и менеджмента команд  License
--- bot/handlers/registration.py
+++ bot/handlers/registration.py
@@ -1,25 +1,30 @@
 import logging
+
 from aiogram import Router, types
 from aiogram.filters import Command
-from config import BASE_URL
 from utils.token_manager import TokenManager
 
 router = Router()
 
 token_manager = TokenManager()  # Создаем экземпляр TokenManager
 
+
 @router.message(Command("start"))
 async def start(message: types.Message):
     command_with_args = message.text.split()
 
     if len(command_with_args) < 2:
-        await message.answer("👋 Привет! Этот бот предназначен для отправки уведомлений участникам хакатонов.")
+        await message.answer(
+            "👋 Привет! Этот бот предназначен для отправки уведомлений участникам хакатонов."
+        )
         return
 
     user_uuid = command_with_args[1]  # Получаем UUID напрямую
 
     if not user_uuid:
-        await message.answer("Упс! 😅 Кажется, не удалось найти тебя. Попробуй еще раз!")
+        await message.answer(
+            "Упс! 😅 Кажется, не удалось найти тебя. Попробуй еще раз!"
+        )
         return
 
     telegram_id = message.from_user.id
@@ -27,16 +32,22 @@ async def start(message: types.Message):
     try:
         response = await token_manager.request_with_token(
             method="POST",
-            url=f"{BASE_URL}link_telegram",
-            params={"user_id": user_uuid, "telegram_id": telegram_id},  # Передаем как параметры запроса
-            headers={"accept": "application/json"}
+            url="/link_telegram",
+            params={
+                "user_id": user_uuid,
+                "telegram_id": telegram_id,
+            },  # Передаем как параметры запроса
+            headers={"accept": "application/json"},
         )
 
         if response.status_code == 200:
             await message.answer(
-                "🎉 Ура! Твой Telegram ID успешно привязан к аккаунту. Теперь мы сможем держать тебя в курсе всех событий! 🚀")
+                "🎉 Ура! Твой Telegram ID успешно привязан к аккаунту. Теперь мы сможем держать тебя в курсе всех событий! 🚀"
+            )
         else:
-            await message.answer("Ой! 😕 Что-то пошло не так... Попробуй еще раз чуть позже.")
+            await message.answer(
+                "Ой! 😕 Что-то пошло не так... Попробуй еще раз чуть позже."
+            )
     except Exception as e:
         logging.error(f"Ошибка: {e}")
         await message.answer("❌ Произошла ошибка. Пожалуйста, попробуйте снова позже.")
