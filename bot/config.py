40ddd0962fc78bf5a7703222fabbc1711f3a3a4e
lysmux <kllraz@yandex.ru>
2024-08-29 10:57:21 +0300

    cd: setup deploy

--- bot/config.py
+++ bot/config.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-EMAIL = os.getenv("EMAIL")
-PASSWORD = os.getenv("PASSWORD")
+SERVICE_EMAIL = os.getenv("SERVICE_EMAIL")
+SERVICE_PASSWORD = os.getenv("SERVICE_PASSWORD")
 
 API_URL = os.getenv("API_URL")
